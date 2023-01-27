c2c24f700cb4bad2f9bad844e7d3acfa8a56b607
gabrielbazante <gabrielbazante7@gmail.com>
2023-01-27 14:48:29 -0300

    Componente Tabela #01

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,6 +1,15 @@
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
+  const clientes = [
+    new Cliente('Gabriel', 28, '1'),
+    new Cliente('Rosita', 25, '2'),
+    new Cliente('Amanda', 28, '3'),
+    new Cliente('Camila', 25, '4'),
+  ]
+
   return (
     <div className={`
       flex h-screen justify-center items-center
@@ -8,7 +17,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
